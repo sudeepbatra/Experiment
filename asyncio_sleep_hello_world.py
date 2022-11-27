2f627acb6128ce543295bb95a3d161b7f7818f29
Sudeep Batra <hftautobot@gmail.com>
2022-11-27 19:20:59 +0530

    Asyncio Sleep Hello World!

---
+++ asyncio_sleep_hello_world.py
@@ -0,0 +1,15 @@
+import asyncio
+
+
+async def hello_world_message() -> str:
+    await asyncio.sleep(10)
+
+    return "Hello World!"
+
+
+async def main() -> None:
+    message = await hello_world_message()
+    print(message)
+
+
+asyncio.run(main())
