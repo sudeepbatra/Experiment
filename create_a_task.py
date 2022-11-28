ed791bce4f06e680ffce81e1a07366853da537f1
Sudeep Batra <hftautobot@gmail.com>
2022-11-28 07:29:41 +0530

    Running multiple tasks concurrently.

--- create_a_task.py
+++ create_a_task.py
@@ -3,7 +3,7 @@ from util import delay
 
 
 async def main():
-    # Create task returns instantly, and starts running the task. await expression will extract the value once it is complete.
+    # Create task returns instantly and schedules it to run as soon as possible. await expression will extract the value once it is complete.
     sleep_for_three = asyncio.create_task(delay(3))
     print(type(sleep_for_three))
     result = await sleep_for_three
