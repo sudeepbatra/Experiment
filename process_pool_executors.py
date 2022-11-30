6d3fb9adba402cbe4779034502e8d5c40a5076a0
Sudeep Batra <hftautobot@gmail.com>
2022-11-30 19:26:38 +0530

    Added Process Pool Executors

---
+++ process_pool_executors.py
@@ -0,0 +1,19 @@
+import time
+from concurrent.futures import ProcessPoolExecutor
+
+
+def count(count_to: int) -> int:
+    start_time = time.time()
+    counter = 0
+    while counter < count_to:
+        counter += 1
+    end_time = time.time()
+    print(f"Finished counting to {count_to} in {end_time - start_time} seconds")
+    return counter
+
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor() as process_pool:
+        numbers = [1, 3, 5, 22, 100000000, 200000000, 500000000, 1000000000, 5000000000]
+        for result in process_pool.map(count, numbers):
+            print(result)
