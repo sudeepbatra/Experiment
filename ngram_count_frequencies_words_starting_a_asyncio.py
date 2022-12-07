88e992be80f59752778534549aad653b1303b819
Sudeep Batra <hftautobot@gmail.com>
2022-12-07 09:29:36 +0530

    Added ngram count frequencies of words starting with letter a with asyncio.

---
+++ ngram_count_frequencies_words_starting_a_asyncio.py
@@ -0,0 +1,53 @@
+import asyncio
+import concurrent.futures
+import functools
+import time
+from typing import List, Dict
+
+
+def partition(data: List, chunk_size: int) -> List:
+    for i in range(0, len(data), chunk_size):
+        yield data[i: i + chunk_size]
+
+
+def map_frequencies(chunk: List[str]) -> Dict[str, int]:
+    counter = {}
+
+    for line in chunk:
+        word, _, count, _ = line.split('\t')
+        if counter.get(word):
+            counter[word] += int(count)
+        else:
+            counter[word] = int(count)
+    return counter
+
+
+def merge_dictionaries(first: Dict[str, int], second: Dict[str, int]):
+    merged = first
+    for key in second:
+        if key in merged:
+            merged[key] = merged[key] + second[key]
+        else:
+            merged[key] = second[key]
+    return merged
+
+
+async def main(partition_size: int):
+    with open('/home/sb/Data/googlebooks-eng-all-1gram-20120701-a', encoding='utf-8') as f:
+        contents = f.readlines()
+        loop = asyncio.get_running_loop()
+        tasks = []
+        start = time.time()
+        with concurrent.futures.ProcessPoolExecutor() as process_pool:
+            for chunk in partition(contents, partition_size):
+                tasks.append(loop.run_in_executor(process_pool, functools.partial(map_frequencies, chunk)))
+
+        intermediate_results = await asyncio.gather(*tasks)
+        final_results = functools.reduce(merge_dictionaries, intermediate_results)
+        print(f"Aardvark has appeared {final_results['Aardvark']} times")
+        end = time.time()
+        print(f"MapReduce took: {end - start:.4f} seconds")
+
+
+if __name__ == '__main__':
+    asyncio.run(main(partition_size=60000))
