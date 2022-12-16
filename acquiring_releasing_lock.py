316a11d0ba98919e73611c0c5e77e8d0398d0d13
Sudeep Batra <hftautobot@gmail.com>
2022-12-16 15:14:40 +0530

    Acquiring and Releasing of locks with Block

--- acquiring_releasing_lock.py
+++ acquiring_releasing_lock.py
@@ -8,11 +8,18 @@ def increment_value(shared_int: Value):
     shared_int.get_lock().release()
 
 
+def increment_value_with_block(shared_int: Value):
+    with shared_int.get_lock():
+        shared_int.value = shared_int.value + 1
+
+
 if __name__ == '__main__':
     for i in range(10000000):
         integer = Value("i", 0)
-        procs = [Process(target=increment_value, args=(integer,)), Process(target=increment_value, args=(integer,)),
-                 Process(target=increment_value, args=(integer,)), Process(target=increment_value, args=(integer,))]
+        procs = [Process(target=increment_value_with_block, args=(integer,)),
+                 Process(target=increment_value_with_block, args=(integer,)),
+                 Process(target=increment_value_with_block, args=(integer,)),
+                 Process(target=increment_value_with_block, args=(integer,))]
         [p.start() for p in procs]
         [p.join() for p in procs]
         print(integer.value)
