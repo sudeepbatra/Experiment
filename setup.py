7c8a31f1709d79687757d75bc1c14d4198bb12c1
Sudeep Batra <hftautobot@gmail.com>
2022-12-18 08:48:23 +0530

    Fibonacci - Cython

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from Cython.Build import cythonize
 from setuptools import setup
 
 setup(
-    ext_modules=cythonize("helloworld.pyx")
+    # ext_modules=cythonize("helloworld.pyx")
+    ext_modules=cythonize("fibonacci_cython.pyx")
 )
