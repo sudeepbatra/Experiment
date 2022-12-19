8977f5e85a81dddecf5a0ce7593a836b96bbb78e
Sudeep Batra <hftautobot@gmail.com>
2022-12-19 10:18:34 +0530

    Prime Cython html.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     # ext_modules=cythonize("helloworld.pyx")
     # ext_modules=cythonize("fibonacci_cython.pyx")
-    ext_modules=cythonize("primes_cython.pyx")
+    ext_modules=cythonize("primes_cython.pyx", annotate=True)
 )
